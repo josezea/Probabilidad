c25c2c92389ef1f2572b5006118827cc2ce83a7c
josezea <jzea@ins.gov.co>
2020-05-28 21:45:49 -0500

    Add files via upload

--- Clase 16 pronostico e intro Regresion logistico.r
+++ Clase 16 pronostico e intro Regresion logistico.r
@@ -1,4 +1,60 @@
 
+
+library(TeachingSampling)
+data(Lucy)
+# Ingreso en término de los impuesto numero de empleados (no ayudaba)
+# Level (Tamaño de empresa)
+
+modelo <- lm(Income ~ Taxes + Level, data = Lucy )
+summary(modelo)
+
+datos <-data.frame(Taxes = c(3.5, 200, 70), 
+                   Level =  c("Small", "Big", "Medium") )
+
+head(predict(modelo, datos))
+438.15 +  10.77 * 3.5 -198.08  
+438.15 +  10.77 * 200   
+438.15 +  10.77 * 70  - 15.04
+
+# En la vida real se quieren hacer pronosticos con otros datos diferentes
+
+## Objetivo: usar la regresión línear para predecir 
+set.seed(12345)
+indica_muestra <- sample(2396, round(2396 * 0.7 ))
+train <- Lucy[indica_muestra,]
+modelo_train <- lm(Income ~ Taxes + Level, data = train)
+summary(modelo)
+
+test <- Lucy[-indica_muestra,]
+# Pronosticos por fuera de muestra
+yhat <- predict(modelo_train, test)
+mae <- mean(abs(test$Income - yhat))
+mae 
+summary(test$Income)
+
+mse <-  mean((test$Income - yhat)^2)          
+#RMSE
+sqrt(mse)
+plot(test$Income,yhat, pch = 20)
+
+# Ejercicio 
+
+# Ajustar un modelo de regresión lineal sobre una muestra de entranamiento 
+# al ingreso en términos de
+# Taxes (una función cuadrática), el timpo de empresa y la zona.
+
+# y ~ x1 + I(x1^2)
+
+#b. Validar en la muestra test que tan bueno son los pronosticos
+#Calcular el mae y rmse y compararlo con el valor promedio del ingreso
+# Mostrar grafico
+
+
+
+# Nuevos datos
+# predict(modelo, datosnuevos)
+
+
 ################# regresión logística ###########################
  # Variables de unos y ceros
 data(iris)
